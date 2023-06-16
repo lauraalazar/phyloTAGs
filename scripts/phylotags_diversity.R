3b953b2d3f15ddc2e30b951843991a68c54e131d
lauraalazar <lauraalazar@gmail.com>
2023-06-16 15:30:54 -0500

    Update phylotags_diversity.R

--- scripts/phylotags_diversity.R
+++ scripts/phylotags_diversity.R
@@ -6,16 +6,41 @@ marker_rare_100 = marker_rare
 
 #ALPHA DIVERSITY
 #Generate a data.frame with alpha div measures
-marker_adiv <- data.frame(
-  "Observed" = phyloseq::estimate_richness(marker_rare, measures = "Observed"),
-  "Shannon" = phyloseq::estimate_richness(marker_rare, measures = "Shannon"),
-  "BMI" = phyloseq::sample_data(marker_rare)$bmi,
-  "bmi_class" = factor(sample_data(marker_rare)$bmi_class,levels = c("Lean","Overweight","Obese")),
-  "city" = phyloseq::sample_data(marker_rare)$city,
-  "sex" = phyloseq::sample_data(marker_rare)$sex,
-  "age_range" = phyloseq::sample_data(marker_rare)$age_range)
+ssu_adiv <- data.frame(
+  "Observed" = phyloseq::estimate_richness(ssu_rare, measures = "Observed"),
+  "Shannon" = phyloseq::estimate_richness(ssu_rare, measures = "Shannon"),
+  #  "PD" = picante::pd(samp = data.frame(t(data.frame(phyloseq::otu_table(dnak_rare)))), tree = phyloseq::phy_tree(dnak_rare))[, 1],
+  #  "CHS" = phyloseq::sample_data(dnak_rare)$chs_class,
+  "BMI" = phyloseq::sample_data(ssu_rare)$bmi,
+  "bmi_class" = factor(sample_data(ssu_rare)$bmi_class,levels = c("Lean","Overweight","Obese")),
+  "city" = phyloseq::sample_data(ssu_rare)$city,
+  "sex" = phyloseq::sample_data(ssu_rare)$sex,
+  "age_range" = phyloseq::sample_data(ssu_rare)$age_range)
 # head(ssu_adiv)
 
+dnak_adiv <- data.frame(
+  "Observed" = phyloseq::estimate_richness(dnak_rare, measures = "Observed"),
+  "Shannon" = phyloseq::estimate_richness(dnak_rare, measures = "Shannon"),
+  #  "PD" = picante::pd(samp = data.frame(t(data.frame(phyloseq::otu_table(dnak_rare)))), tree = phyloseq::phy_tree(dnak_rare))[, 1],
+  #  "CHS" = phyloseq::sample_data(dnak_rare)$chs_class,
+  "BMI" = phyloseq::sample_data(dnak_rare)$bmi,
+  "bmi_class" = factor(sample_data(dnak_rare)$bmi_class,levels = c("Lean","Overweight","Obese")),
+  "city" = phyloseq::sample_data(dnak_rare)$city,
+  "sex" = phyloseq::sample_data(dnak_rare)$sex,
+  "age_range" = phyloseq::sample_data(dnak_rare)$age_range)
+# head(dnak_adiv)
+
+gyrB_adiv <- data.frame(
+  "Observed" = phyloseq::estimate_richness(gyrB_rare, measures = "Observed"),
+  "Shannon" = phyloseq::estimate_richness(gyrB_rare, measures = "Shannon"),
+  #  "PD" = picante::pd(samp = data.frame(t(data.frame(phyloseq::otu_table(dnak_rare)))), tree = phyloseq::phy_tree(dnak_rare))[, 1],
+  #  "CHS" = phyloseq::sample_data(dnak_rare)$chs_class,
+  "BMI" = phyloseq::sample_data(gyrB_rare)$bmi,
+  "bmi_class" = factor(sample_data(gyrB_rare)$bmi_class,levels = c("Lean","Overweight","Obese")),
+  "city" = phyloseq::sample_data(gyrB_rare)$city,
+  "sex" = phyloseq::sample_data(gyrB_rare)$sex,
+  "age_range" = phyloseq::sample_data(gyrB_rare)$age_range)
+# head(gyrB_adiv)
 
 ## SUMMARY OF SHANNON STATS FOR DIFFERENT TAXA
 
@@ -52,7 +77,7 @@ marker_tax_stats = function(marker=NULL,clustering=NULL,level_tax=NULL){
   shannonbinary_family.df = data.frame(matrix(nrow=ncol(otu_table(phyobj)),ncol=0))
   observed_family.df = data.frame(matrix(nrow=ncol(otu_table(phyobj)),ncol=0))
   
-  #loop over each taxón and calculate de shannon for each sample, then bind to a dataframe
+  #loop over each taxÃ³n and calculate de shannon for each sample, then bind to a dataframe
   family_names = NULL
   for (tx in taxa_list){
     #  print(tx)
@@ -98,39 +123,80 @@ marker_tax_stats = function(marker=NULL,clustering=NULL,level_tax=NULL){
 }
 
 ## ANOVA
-marker_shannon.fam <- marker_tax_stats("marker",idlevel,"family")$shannon
-
+#ssu
+ssu_shannon.fam <- marker_tax_stats("ssu",idlevel,"family")$shannon
 #Check the means for each group to know direction 
 #marker_shannon.fam  %>% 
 #select(CAG_302,bmi_class) %>% 
 #  group_by(bmi_class) %>% 
 #  summarize(Mean = mean(CAG_302, na.rm=TRUE))
 
-marker_shannon.gen <- marker_tax_stats("marker",idlevel,"genus")$shannon
+ssu_shannon.gen <- marker_tax_stats("ssu",idlevel,"genus")$shannon
 
-marker_shannon.fam.aov <- lapply(setdiff(colnames(marker_shannon.fam), c("bmi","bmi_class","city")), 
+ssu_shannon.fam.aov <- lapply(setdiff(colnames(ssu_shannon.fam), c("bmi","bmi_class","city")),
                               function(s) {aov(as.formula(paste(as.character(s), " ~ bmi_class")),ssu_shannon.fam)})
-marker_shannon.gen.aov <- lapply(setdiff(colnames(marker_shannon.gen), c("bmi","bmi_class","city")), 
+ssu_shannon.gen.aov <- lapply(setdiff(colnames(ssu_shannon.gen), c("bmi","bmi_class","city")),
                               function(s) {aov(as.formula(paste(as.character(s), " ~ bmi_class")),ssu_shannon.gen)})
-
 
 #simple way to do it
 #lapply(marker_shannon.fam.aov,function(x) {summary(x)})
 
 #more fancy but gives a list of all statistics for all families
-marker_vars.fam = names(marker_shannon.fam)[!names(marker_shannon.fam) %in% c("bmi","bmi_class","city")]
-marker_vars.gen = names(marker_shannon.gen)[!names(marker_shannon.gen) %in% c("bmi","bmi_class","city")]
+ssu_vars.fam = names(ssu_shannon.fam)[!names(ssu_shannon.fam) %in% c("bmi","bmi_class","city")]
+ssu_vars.gen = names(ssu_shannon.gen)[!names(ssu_shannon.gen) %in% c("bmi","bmi_class","city")]
 
-marker_anova_fam.df = lapply(setNames(marker_vars.fam, marker_vars.fam),function(s) {
-  aov(as.formula(paste(as.character(s), " ~ bmi_class")),marker_shannon.fam)})
+ssu_anova_fam.df = lapply(setNames(ssu_vars.fam, ssu_vars.fam),function(s) {
+  aov(as.formula(paste(as.character(s), " ~ bmi_class")),ssu_shannon.fam)})
 
-marker_anova_gen.df = lapply(setNames(marker_vars.gen, marker_vars.gen),function(s) {
-  aov(as.formula(paste(as.character(s), " ~ bmi_class")),marker_shannon.gen)})
+ssu_anova_gen.df = lapply(setNames(ssu_vars.gen, ssu_vars.gen),function(s) {
+  aov(as.formula(paste(as.character(s), " ~ bmi_class")),ssu_shannon.gen)})
 
 #significant table
-marker_anova_fam.sigtab <- map_df(marker_anova_fam.df, tidy, .id="taxa")[which(map_df(marker_anova_fam.df, tidy, .id="taxa")[,"p.value"]<0.05),]
-marker_anova_gen.sigtab <- map_df(marker_anova_gen.df, tidy, .id="taxa")[which(map_df(marker_anova_gen.df, tidy, .id="taxa")[,"p.value"]<0.05),]
+ssu_anova_fam.sigtab <- map_df(ssu_anova_fam.df, tidy, .id="taxa")[which(map_df(ssu_anova_fam.df, tidy, .id="taxa")[,"p.value"]<0.05$
+ssu_anova_gen.sigtab <- map_df(ssu_anova_gen.df, tidy, .id="taxa")[which(map_df(ssu_anova_gen.df, tidy, .id="taxa")[,"p.value"]<0.05$
 
+#dnaK
+dnak_shannon.fam.aov <- lapply(setdiff(colnames(dnak_shannon.fam), c("bmi","bmi_class","city")),
+                               function(s) {aov(as.formula(paste(as.character(s), " ~ bmi_class")),dnak_shannon.fam)})
+dnak_shannon.gen.aov <- lapply(setdiff(colnames(dnak_shannon.gen), c("bmi","bmi_class","city")),
+                               function(s) {aov(as.formula(paste(as.character(s), " ~ bmi_class")),dnak_shannon.gen)})
+
+dnak_vars.fam = names(dnak_shannon.fam)[!names(dnak_shannon.fam) %in% c("bmi","bmi_class","city")]
+dnak_vars.gen = names(dnak_shannon.gen)[!names(dnak_shannon.gen) %in% c("bmi","bmi_class","city")]
+
+dnak_anova_fam.df = lapply(setNames(dnak_vars.fam, dnak_vars.fam),function(s) {
+  aov(as.formula(paste(as.character(s), " ~ bmi_class")),dnak_shannon.fam)})
+dnak_anova_gen.df = lapply(setNames(dnak_vars.gen, dnak_vars.gen),function(s) {
+  aov(as.formula(paste(as.character(s), " ~ bmi_class")),dnak_shannon.gen)})
+
+#significant table
+dnak_anova_fam.sigtab <- map_df(dnak_anova_fam.df, tidy, .id="taxa")[which(map_df(dnak_anova_fam.df, tidy, .id="taxa")[,"p.value"]<0$
+dnak_anova_gen.sigtab <- map_df(dnak_anova_gen.df, tidy, .id="taxa")[which(map_df(dnak_anova_gen.df, tidy, .id="taxa")[,"p.value"]<0$
+
+#gyrB
+#gyrB_rare_100 <- gyrB_rare
+gyrB_shannon.fam <- marker_tax_stats("gyrB",idlevel,"family")$shannon
+gyrB_shannon.gen <- marker_tax_stats("gyrB",idlevel,"genus")$shannon
+  
+
+gyrB_shannon.fam.aov <- lapply(setdiff(colnames(gyrB_shannon.fam), c("bmi","bmi_class","city")),
+                               function(s) {aov(as.formula(paste(as.character(s), " ~ bmi_class")),gyrB_shannon.fam)})
+gyrB_shannon.gen.aov <- lapply(setdiff(colnames(gyrB_shannon.gen), c("bmi","bmi_class","city")),
+                               function(s) {aov(as.formula(paste(as.character(s), " ~ bmi_class")),gyrB_shannon.gen)})
+
+gyrB_vars.fam = names(gyrB_shannon.fam)[!names(gyrB_shannon.fam) %in% c("bmi","bmi_class","city")]
+gyrB_vars.gen = names(gyrB_shannon.gen)[!names(gyrB_shannon.gen) %in% c("bmi","bmi_class","city")]
+
+
+gyrB_anova_fam.df = lapply(setNames(gyrB_vars.fam, gyrB_vars.fam),function(s) {
+  aov(as.formula(paste(as.character(s), " ~ bmi_class")),gyrB_shannon.fam)})
+gyrB_anova_gen.df = lapply(setNames(gyrB_vars.gen, gyrB_vars.gen),function(s) {
+  aov(as.formula(paste(as.character(s), " ~ bmi_class")),gyrB_shannon.gen)})
+  
+#significant table
+gyrB_anova_fam.sigtab <- map_df(gyrB_anova_fam.df, tidy, .id="taxa")[which(map_df(gyrB_anova_fam.df, tidy, .id="taxa")[,"p.value"]<0$
+gyrB_anova_gen.sigtab <- map_df(gyrB_anova_gen.df, tidy, .id="taxa")[which(map_df(gyrB_anova_gen.df, tidy, .id="taxa")[,"p.value"]<0$
+                                                                         
 # Gamma-hurdle (zero and non-zero modelled "indepentently")
 #https://seananderson.ca/2014/05/18/gamma-hurdle/
 #two models that give 1) m1: th probability of non-zero 
