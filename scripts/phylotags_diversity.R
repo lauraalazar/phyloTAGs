7c0735af63793e387c2bb580d0e6b89694e1b026
lauraalazar <lauraalazar@gmail.com>
2023-06-16 15:56:40 -0500

    Update phylotags_diversity.R

--- scripts/phylotags_diversity.R
+++ scripts/phylotags_diversity.R
@@ -206,70 +206,172 @@ gyrB_anova_gen.sigtab <- map_df(gyrB_anova_gen.df, tidy, .id="taxa")[which(map_d
 
 ## BINARY
 #ssu
-marker_shannon_bin.fam <- marker_tax_stats("marker",idlevel,"family")$shannon_bin
-marker_shannon_bin.gen <- marker_tax_stats("marker",idlevel,"genus")$shannon_bin
+ssu_shannon_bin.fam <- marker_tax_stats("ssu",idlevel,"family")$shannon_bin
+ssu_shannon_bin.gen <- marker_tax_stats("ssu",idlevel,"genus")$shannon_bin
 
-marker_vars.fam = names(marker_shannon_bin.fam)[!names(marker_shannon_bin.fam) %in% c("bmi","bmi_class","city")]
-marker_vars.gen = names(marker_shannon_bin.gen)[!names(marker_shannon_bin.gen) %in% c("bmi","bmi_class","city")]
+ssu_vars.fam = names(ssu_shannon_bin.fam)[!names(ssu_shannon_bin.fam) %in% c("bmi","bmi_class","city")]
+ssu_vars.gen = names(ssu_shannon_bin.gen)[!names(ssu_shannon_bin.gen) %in% c("bmi","bmi_class","city")]
+                               
+ssu_glmbin_fam.df <- lapply(setNames(ssu_vars.fam, ssu_vars.fam),function(s) {
+  glm(as.formula(paste(as.character(s), " ~ bmi + city")),
+      data = ssu_shannon_bin.fam, family = binomial(link = logit))})
+ssu_glmbin_gen.df <- lapply(setNames(ssu_vars.gen, ssu_vars.gen),function(s) {
+  glm(as.formula(paste(as.character(s), " ~ bmi + city")),
+      data = ssu_shannon_bin.gen, family = binomial(link = logit))})
+  
+ssu_glmbin_fam.mapdf <- map_df(ssu_glmbin_fam.df, tidy, .id="taxa")
+ssu_glmbin_gen.mapdf <- map_df(ssu_glmbin_gen.df, tidy, .id="taxa")
 
-marker_glmbin_fam.df <- lapply(setNames(marker_vars.fam, marker_vars.fam),function(s) {
-  glm(as.formula(paste(as.character(s), " ~ bmi + city")), 
-      data = marker_shannon_bin.fam, family = binomial(link = logit))})
-ssu_glmbin_gen.df <- lapply(setNames(marker_vars.gen, marker_vars.gen),function(s) {
-  glm(as.formula(paste(as.character(s), " ~ bmi + city")), 
-      data = marker_shannon_bin.gen, family = binomial(link = logit))})
-
-marker_glmbin_fam.mapdf <- map_df(marker_glmbin_fam.df, tidy, .id="taxa")
-marker_glmbin_gen.mapdf <- map_df(marker_glmbin_gen.df, tidy, .id="taxa")
-
-marker_glmbin_fam.sigtab <- marker_glmbin_fam.mapdf %>%
+ssu_glmbin_fam.sigtab <- ssu_glmbin_fam.mapdf %>%
   filter(term=="bmi" & p.value<0.05)
-marker_glmbin_gen.sigtab <- ssu_glmbin_gen.mapdf %>%
+ssu_glmbin_gen.sigtab <- ssu_glmbin_gen.mapdf %>%
+  filter(term=="bmi" & p.value<0.05)## CONTINOUS
+
+#dnak
+dnak_shannon_bin.fam <- marker_tax_stats("dnak",idlevel,"family")$shannon_bin
+dnak_vars.fam = names(dnak_shannon_bin.fam)[!names(dnak_shannon_bin.fam) %in% c("bmi","bmi_class","city")]
+
+dnak_shannon_bin.gen <- marker_tax_stats("dnak",idlevel,"genus")$shannon_bin
+
+dnak_vars.fam = names(dnak_shannon_bin.fam)[!names(dnak_shannon_bin.fam) %in% c("bmi","bmi_class","city")]
+dnak_vars.gen = names(dnak_shannon_bin.gen)[!names(dnak_shannon_bin.gen) %in% c("bmi","bmi_class","city")]
+
+dnak_glmbin_fam.df <- lapply(setNames(dnak_vars.fam, dnak_vars.fam),function(s) {
+  glm(as.formula(paste(as.character(s), " ~ bmi + city")),
+      data = dnak_shannon_bin.fam, family = binomial(link = logit))})
+dnak_glmbin_gen.df <- lapply(setNames(dnak_vars.gen, dnak_vars.gen),function(s) {
+  glm(as.formula(paste(as.character(s), " ~ bmi + city")),
+      data = dnak_shannon_bin.gen, family = binomial(link = logit))})
+
+dnak_glmbin_fam.mapdf <- map_df(dnak_glmbin_fam.df, tidy, .id="taxa")
+dnak_glmbin_gen.mapdf <- map_df(dnak_glmbin_gen.df, tidy, .id="taxa")
+
+dnak_glmbin_fam.sigtab <- dnak_glmbin_fam.mapdf %>%
+  filter(term=="bmi" & p.value<0.05)
+dnak_glmbin_gen.sigtab <- dnak_glmbin_gen.mapdf %>%
   filter(term=="bmi" & p.value<0.05)
 
+#gyrB
+gyrB_shannon_bin.fam <- marker_tax_stats("gyrB",idlevel,"family")$shannon_bin
+gyrB_shannon_bin.gen <- marker_tax_stats("gyrB",idlevel,"genus")$shannon_bin
+      
+gyrB_vars.fam = names(gyrB_shannon_bin.fam)[!names(gyrB_shannon_bin.fam) %in% c("bmi","bmi_class","city")]
+gyrB_vars.gen = names(gyrB_shannon_bin.gen)[!names(gyrB_shannon_bin.gen) %in% c("bmi","bmi_class","city")]
+
+gyrB_glmbin_fam.df <- lapply(setNames(gyrB_vars.fam, gyrB_vars.fam),function(s) {
+  glm(as.formula(paste(as.character(s), " ~ bmi + city")),
+      data = gyrB_shannon_bin.fam, family = binomial(link = logit))})
+gyrB_glmbin_gen.df <- lapply(setNames(gyrB_vars.gen, gyrB_vars.gen),function(s) {
+  glm(as.formula(paste(as.character(s), " ~ bmi + city")),
+      data = gyrB_shannon_bin.gen, family = binomial(link = logit))})
+
+gyrB_glmbin_fam.mapdf <- map_df(gyrB_glmbin_fam.df, tidy, .id="taxa")
+gyrB_glmbin_gen.mapdf <- map_df(gyrB_glmbin_gen.df, tidy, .id="taxa")
+  
+gyrB_glmbin_fam.sigtab <- gyrB_glmbin_fam.mapdf %>%
+  filter(term=="bmi" & p.value<0.05)
+gyrB_glmbin_gen.sigtab <- gyrB_glmbin_gen.mapdf %>%
+  filter(term=="bmi" & p.value<0.05)
+                                                                           
+                                                                           
 ## CONTINOUS
+
+#ssu
+# first remove columns where all values are 0 (for numeric values)   
+ssu_shannon_cont.fam <- ssu_shannon.fam %>%
+  select_if(~ !is.numeric(.) || sum(.) != 0)
+ssu_shannon_cont.gen <- ssu_shannon.gen %>%
+  select_if(~ !is.numeric(.) || sum(.) != 0)
+
+ssu_vars.fam = names(ssu_shannon_cont.fam)[!names(ssu_shannon_cont.fam) %in% c("bmi","bmi_class","city")] #,"Enterococcaceae","Parachla$
+ssu_vars.gen = names(ssu_shannon_cont.gen)[!names(ssu_shannon_cont.gen) %in% c("bmi","bmi_class","city")]
+#non_zero <- ssu_shannon_cont.fam[ssu_shannon_cont.fam[which(colnames(ssu_shannon_cont.fam)==s)]!=0,]
+  
+# second drop also variables with less than 3 values (at least two values different to 0), otherwise there will be errors when attempti$
+# regression model using a predictor variable with 2 or less values
+ssu_keep.fam <- names(which(sapply(lapply(ssu_shannon_cont.fam[,ssu_vars.fam], unique), length) > 3))
+ssu_keep.gen <- names(which(sapply(lapply(ssu_shannon_cont.gen[,ssu_vars.gen], unique), length) > 3))
+
+
+ssu_glmcont_fam.df <- lapply(setNames(ssu_keep.fam, ssu_keep.fam),function(s) {
+  glm(as.formula(paste(as.character(s), " ~ bmi + city")),
+      data = ssu_shannon_cont.fam[ssu_shannon_cont.fam[,s]!=0,], family = Gamma(link = log))})
+
+ssu_glmcont_gen.df <- lapply(setNames(ssu_keep.gen, ssu_keep.gen),function(s) {
+  glm(as.formula(paste(as.character(s), " ~ bmi  + city")),
+       data = ssu_shannon_cont.gen[ssu_shannon_cont.gen[which(colnames(ssu_shannon_cont.gen)==s)]!=0,], family = Gamma(link = log))})
+
+ssu_glmcont_fam.mapdf <- map_df(ssu_glmcont_fam.df, tidy, .id="taxa")
+ssu_glmcont_gen.mapdf <- map_df(ssu_glmcont_gen.df, tidy, .id="taxa")
+
+ssu_glmcont_fam.sigtab <- ssu_glmcont_fam.mapdf %>%
+  filter(term=="bmi" & p.value<0.055)
+ssu_glmcont_gen.sigtab <- ssu_glmcont_gen.mapdf %>%
+  filter(term=="bmi" & p.value<0.055)
+
+#dnak 
 # first remove columns where all values are 0 (for numeric values)
-marker_shannon_cont.fam <- marker_shannon.fam %>% 
+dnak_shannon_cont.fam <- dnak_shannon.fam %>%
   select_if(~ !is.numeric(.) || sum(.) != 0)
-marker_shannon_cont.gen <- marker_shannon.gen %>% 
+dnak_shannon_cont.gen <- dnak_shannon.gen %>%
   select_if(~ !is.numeric(.) || sum(.) != 0)
 
-marker_vars.fam = names(marker_shannon_cont.fam)[!names(marker_shannon_cont.fam) %in% c("bmi","bmi_class","city")] 
-marker_vars.gen = names(marker_shannon_cont.gen)[!names(marker_shannon_cont.gen) %in% c("bmi","bmi_class","city")]
+dnak_vars.fam = names(dnak_shannon_cont.fam)[!names(dnak_shannon_cont.fam) %in% c("bmi","bmi_class","city")]
+dnak_vars.gen = names(dnak_shannon_cont.gen)[!names(dnak_shannon_cont.gen) %in% c("bmi","bmi_class","city")]
 
-marker_glmcont_fam.df <- lapply(setNames(marker_vars.fam, marker_vars.fam),function(s) {
-  glm(as.formula(paste(as.character(s), " ~ bmi ")), 
-      data = marker_shannon_cont.fam[marker_shannon_cont.fam[which(colnames(marker_shannon_cont.fam)==s)]!=0,], family = Gamma(link = log))})
-marker_glmcont_gen.df <- lapply(setNames(marker_vars.gen, marker_vars.gen),function(s) {
-  glm(as.formula(paste(as.character(s), " ~ bmi ")), 
-      data = marker_shannon_cont.gen[marker_shannon_cont.gen[which(colnames(marker_shannon_cont.gen)==s)]!=0,], family = Gamma(link = log))})
+# second drop also variables with less than 3 values (at least two values different to 0), otherwise there will be errors when attempti$
+# regression model using a predictor variable with 2 or less values
+dnak_keep.fam <- names(which(sapply(lapply(dnak_shannon_cont.fam[,dnak_vars.fam], unique), length) > 3))
+dnak_keep.gen <- names(which(sapply(lapply(dnak_shannon_cont.gen[,dnak_vars.gen], unique), length) > 3))
 
-marker_glmcont_fam.mapdf <- map_df(marker_glmcont_fam.df, tidy, .id="taxa")
-marker_glmcont_gen.mapdf <- map_df(marker_glmcont_gen.df, tidy, .id="taxa")
 
-marker_glmcont_fam.sigtab <- marker_glmcont_fam.mapdf %>%
+dnak_glmcont_fam.df <- lapply(setNames(dnak_keep.fam, dnak_keep.fam),function(s) {
+  glm(as.formula(paste(as.character(s), " ~ bmi + city")),
+      data = dnak_shannon_cont.fam[dnak_shannon_cont.fam[,s]!=0,], family = Gamma(link = log))})
+dnak_glmcont_gen.df <- lapply(setNames(dnak_keep.gen, dnak_keep.gen),function(s) {
+  glm(as.formula(paste(as.character(s), " ~ bmi + city")),
+      data = dnak_shannon_cont.gen[dnak_shannon_cont.gen[,s]!=0,], family = Gamma(link = log))})
+  
+dnak_glmcont_fam.mapdf <- map_df(dnak_glmcont_fam.df, tidy, .id="taxa")
+dnak_glmcont_gen.mapdf <- map_df(dnak_glmcont_gen.df, tidy, .id="taxa")
+
+dnak_glmcont_fam.sigtab <- dnak_glmcont_fam.mapdf %>%
+  filter(term=="bmi" & p.value<0.055) # Consider taking an estimate larger than a value to avoid weird patterns
+dnak_glmcont_gen.sigtab <- dnak_glmcont_gen.mapdf %>%
+  filter(term=="bmi" & p.value<0.055) # Consider taking an estimate larger than a value to avoid weird patterns
+
+#gyrB
+# first remove columns where all values are 0 (for numeric values)
+gyrB_shannon_cont.fam <- gyrB_shannon.fam %>%
+  select_if(~ !is.numeric(.) || sum(.) != 0)
+gyrB_shannon_cont.gen <- gyrB_shannon.gen %>%
+  select_if(~ !is.numeric(.) || sum(.) != 0)
+      
+gyrB_vars.fam = names(gyrB_shannon_cont.fam)[!names(gyrB_shannon_cont.fam) %in% c("bmi","bmi_class","city")] #
+gyrB_vars.gen = names(gyrB_shannon_cont.gen)[!names(gyrB_shannon_cont.gen) %in% c("bmi","bmi_class","city")]
+
+# second drop also variables with less than 3 values (at least two values different to 0), otherwise there will be errors when attempti$
+# regression model using a predictor variable with 2 or less values
+gyrB_keep.fam <- names(which(sapply(lapply(gyrB_shannon_cont.fam[,gyrB_vars.fam], unique), length) > 3))
+gyrB_keep.gen <- names(which(sapply(lapply(gyrB_shannon_cont.gen[,gyrB_vars.gen], unique), length) > 3))
+  
+gyrB_glmcont_fam.df <- lapply(setNames(gyrB_keep.fam, gyrB_keep.fam),function(s) {
+  glm(as.formula(paste(as.character(s), " ~ bmi + city")),
+      data = gyrB_shannon_cont.fam[gyrB_shannon_cont.fam[,s]!=0,], family = Gamma(link = log))})
+gyrB_glmcont_gen.df <- lapply(setNames(gyrB_keep.gen, gyrB_keep.gen),function(s) {
+  glm(as.formula(paste(as.character(s), " ~ bmi ")),
+      data = gyrB_shannon_cont.gen[gyrB_shannon_cont.gen[,s]!=0,], family = Gamma(link = log))})
+
+  
+gyrB_glmcont_fam.mapdf <- map_df(gyrB_glmcont_fam.df, tidy, .id="taxa")
+gyrB_glmcont_gen.mapdf <- map_df(gyrB_glmcont_gen.df, tidy, .id="taxa")
+
+gyrB_glmcont_fam.sigtab <- gyrB_glmcont_fam.mapdf %>%
   filter(term=="bmi" & p.value<0.055)
-marker_glmcont_gen.sigtab <- marker_glmcont_gen.mapdf %>%
+gyrB_glmcont_gen.sigtab <- gyrB_glmcont_gen.mapdf %>%
   filter(term=="bmi" & p.value<0.055)
 
-#take significant pvalues for bmi (slope) and run again the model with city
-newvars.fam = marker_glmcont_fam.sigtab$taxa
-newvars.gen = marker_glmcont_gen.sigtab$taxa
-
-marker_glmcont_city_fam.df <- lapply(setNames(newvars.fam, newvars.fam),function(s) {
-  glm(as.formula(paste(as.character(s), " ~ bmi + city")), 
-      data = marker_shannon_cont.fam[marker_shannon_cont.fam[which(colnames(marker_shannon_cont.fam)==s)]!=0,], family = Gamma(link = log))})
-marker_glmcont_city_fam.mapdf <- map_df(marker_glmcont_city_fam.df, tidy, .id="taxa")
-marker_glmcont_city_fam.sigtab <- marker_glmcont_city_fam.mapdf %>%
-  filter(term=="bmi" & p.value<0.055)
-
-marker_glmcont_city_gen.df <- lapply(setNames(newvars.gen, newvars.gen),function(s) {
-  glm(as.formula(paste(as.character(s), " ~ bmi + city")), 
-      data = marker_shannon_cont.gen[marker_shannon_cont.gen[which(colnames(marker_shannon_cont.gen)==s)]!=0,], family = Gamma(link = log))})
-marker_glmcont_city_gen.mapdf <- map_df(marker_glmcont_city_gen.df, tidy, .id="taxa")
-marker_glmcont_city_gen.sigtab <- marker_glmcont_city_gen.mapdf %>%
-  filter(term=="bmi" & p.value<0.055)
-
+                                                                           
 # Correct for multiple testing for all genera within each family
 # subset genera of the family Lachnospiracea
 #gyrB
@@ -292,10 +394,10 @@ dnak_glmcont_gen.oscillo_dnak <- dnak_glmcont_gen.mapdf%>%
   filter(taxa %in% oscillo_dnak & term=="bmi") %>%
   mutate(padj=p.adjust(p.value,method = "fdr"))
 #bin
-dnak_glmbin_gen.oscillo_dnak <- dnak_glmbin_gen.mapdf%>%
+dnak_glmbin_gen.oscillo_dnak <- dnak_glmbin_gen.mapdf%>% 
   filter(taxa %in% oscillo_dnak & term=="bmi") %>%
   mutate(padj=p.adjust(p.value,method = "fdr"))
-#g__CAG_170 (0.0196)
+#g__CAG_170 (0.0196) 
 #g__CAG_83 (0.182)
 #g__ER4 (0.236)
 
@@ -310,35 +412,46 @@ dnak_glmcont_gen.acuta_dnak <- dnak_glmcont_gen.mapdf%>%
 dnak_glmbin_gen.acuta_dnak <- dnak_glmbin_gen.mapdf%>%
   filter(taxa %in% acuta_dnak & term=="bmi") %>%
   mutate(padj=p.adjust(p.value,method = "fdr"))
-
+  
 rumino_dnak <- levels(as.factor(dnak_taxmat_dada2[which(dnak_taxmat_dada2[,"family"] %in% "f__Ruminococcaceae"),"genus"]))
 rumino_dnak <- str_replace_all(rumino_dnak, "-","_")
 #cont
 dnak_glmcont_gen.rumino_dnak <- dnak_glmcont_gen.mapdf%>%
   filter(taxa %in% rumino_dnak & term=="bmi") %>%
   mutate(padj=p.adjust(p.value,method = "fdr"))
-#bin
-dnak_glmbin_gen.rumino_dnak <- dnak_glmbin_gen.mapdf%>%
-  filter(taxa %in% rumino_dnak & term=="bmi") %>%
-  mutate(padj=p.adjust(p.value,method = "fdr"))
 
+#bin
 #ssu
 lachno_ssu <- levels(as.factor(ssu_taxmat_dada2[which(ssu_taxmat_dada2[,"Family"] %in% "Lachnospiraceae"),"Genus"]))
 lachno_ssu <- str_replace_all(lachno_ssu, "-","_")
-ssu_glmcont_gen.lachno_ssu <- ssu_glmcont_gen.mapdf%>%
+ssu_glmbin_gen.lachno_ssu <- ssu_glmbin_gen.mapdf%>%
   filter(taxa %in% lachno_ssu & term=="bmi") %>%
   mutate(padj=p.adjust(p.value,method = "fdr"))
 
-cag138_ssu <- levels(as.factor(ssu_taxmat_dada2[which(ssu_taxmat_dada2[,"Family"] %in% "CAG_138"),"Genus"]))
+oscillo_ssu <- levels(as.factor(ssu_taxmat_dada2[which(ssu_taxmat_dada2[,"Family"] %in% "Oscillospiraceae"),"Genus"]))
+oscillo_ssu <- str_replace_all(oscillo_ssu, "-","_")
+ssu_glmbin_gen.oscillo_ssu <- ssu_glmbin_gen.mapdf%>%
+  filter(taxa %in% oscillo_ssu & term=="bmi") %>%
+  mutate(padj=p.adjust(p.value,method = "fdr"))
+
+  
+cag138_ssu <- levels(as.factor(ssu_taxmat_dada2[which(ssu_taxmat_dada2[,"Family"] %in% "CAG-138"),"Genus"]))
 cag138_ssu <- str_replace_all(cag138_ssu, "-","_")
-cag138_glmcont_gen.cag138_ssu <- ssu_glmcont_gen.mapdf%>%
+cag138_glmbin_gen.cag138_ssu <- ssu_glmbin_gen.mapdf%>%
   filter(taxa %in% cag138_ssu & term=="bmi") %>%
   mutate(padj=p.adjust(p.value,method = "fdr"))
 
 cag74_ssu <- levels(as.factor(ssu_taxmat_dada2[which(ssu_taxmat_dada2[,"Family"] %in% "CAG-74"),"Genus"]))
 cag74_ssu <- str_replace_all(cag74_ssu, "-","_")
-cag74_glmcont_gen.cag74_ssu <- ssu_glmcont_gen.mapdf%>%
+cag74_glmbin_gen.cag74_ssu <- ssu_glmbin_gen.mapdf%>%
   filter(taxa %in% cag74_ssu & term=="bmi") %>%
+  mutate(padj=p.adjust(p.value,method = "fdr"))
+
+                                                                           
+UBA1381_ssu <- levels(as.factor(ssu_taxmat_dada2[which(ssu_taxmat_dada2[,"Family"] %in% "UBA1381"),"Genus"]))
+UBA1381_ssu <- str_replace_all(UBA1381_ssu, "-","_")
+UBA1381_glmbin_gen.UBA1381_ssu <- ssu_glmbin_gen.mapdf%>%
+  filter(taxa %in% UBA1381_ssu & term=="bmi") %>%
   mutate(padj=p.adjust(p.value,method = "fdr"))
 
 #cont
@@ -346,7 +459,15 @@ oscillo_ssu <- levels(as.factor(ssu_taxmat_dada2[which(ssu_taxmat_dada2[,"Family
 oscillo_ssu <- str_replace_all(oscillo_ssu, "-","_")
 ssu_glmcont_gen.oscillo_ssu <- ssu_glmcont_gen.mapdf%>%
   filter(taxa %in% oscillo_ssu & term=="bmi") %>%
+  mutate(padj=p.adjust(p.value,method = "fdr")) 
+
+acuta_ssu <- levels(as.factor(ssu_taxmat_dada2[which(ssu_taxmat_dada2[,"Family"] %in% "Acutalibacteraceae"),"Genus"]))
+acuta_ssu <- str_replace_all(acuta_ssu, "-","_")
+acuta_glmcont_gen.acuta_ssu <- ssu_glmcont_gen.mapdf%>%
+  filter(taxa %in% acuta_ssu & term=="bmi") %>%
   mutate(padj=p.adjust(p.value,method = "fdr"))
+
+
 #bin
 dnak_glmbin_gen.oscillo_dnak <- dnak_glmbin_gen.mapdf%>%
   filter(taxa %in% oscillo_dnak & term=="bmi") %>%
@@ -366,7 +487,7 @@ dnak_glmcont_gen.acuta_dnak <- dnak_glmcont_gen.mapdf%>%
 dnak_glmbin_gen.acuta_dnak <- dnak_glmbin_gen.mapdf%>%
   filter(taxa %in% acuta_dnak & term=="bmi") %>%
   mutate(padj=p.adjust(p.value,method = "fdr"))
-
+  
 rumino_dnak <- levels(as.factor(dnak_taxmat_dada2[which(dnak_taxmat_dada2[,"family"] %in% "f__Ruminococcaceae"),"genus"]))
 rumino_dnak <- str_replace_all(rumino_dnak, "-","_")
 #cont
@@ -376,5 +497,6 @@ dnak_glmcont_gen.rumino_dnak <- dnak_glmcont_gen.mapdf%>%
 #bin
 dnak_glmbin_gen.rumino_dnak <- dnak_glmbin_gen.mapdf%>%
   filter(taxa %in% rumino_dnak & term=="bmi") %>%
-  mutate(padj=p.adjust(p.value,method = "fdr"))
+  mutate(padj=p.adjust(p.value,method = "fdr")) 
 
+                                                                           
